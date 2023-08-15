56ac5d3f955b60526b2f4bd7a83c1e4e34454362
BastianZim <10774221+BastianZim@users.noreply.github.com>
2023-08-15 14:38:26 -0400

    Fix

--- github_handler.py
+++ github_handler.py
@@ -6,19 +6,21 @@ def fetch_prs():
     labels = ["python", "review-requested"]
     label_args = " ".join([f"--label {label}" for label in labels])
 
-    # Include the created field to sort by creation date
-    command = f"gh pr list -R {repo_name} -s open --json number,title,url,created {label_args}"
+    # Include the createdAt field to sort by creation date
+    command = f"gh pr list -R {repo_name} -s open --json number,title,url,createdAt {label_args}"
     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
     prs_json = result.stdout
+
+    # Check if the result is empty or not a valid JSON
+    if prs_json.strip() == '':
+        return []
 
     prs = json.loads(prs_json)
 
     # Sort by creation date and take the first 20
-    sorted_prs = sorted(prs, key=lambda x: x['created'])[:20]
+    sorted_prs = sorted(prs, key=lambda x: x['createdAt'])[:20]
     
     return sorted_prs
-
-
 
 if __name__ == "__main__":
     prs = fetch_prs()
