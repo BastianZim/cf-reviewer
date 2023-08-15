cf6bc59b3c642f24714cb0b1f2767d266303e6cc
BastianZim <10774221+BastianZim@users.noreply.github.com>
2023-08-15 14:34:39 -0400

    Fix labels

--- github_handler.py
+++ github_handler.py
@@ -4,14 +4,15 @@ import json
 def fetch_prs():
     repo_name = "conda-forge/staged-recipes"
     labels = ["python", "review-requested"]
-    label_query = " ".join([f"label:{label}" for label in labels])
+    label_args = " ".join([f"--label {label}" for label in labels])
 
-    command = f"gh pr list -R {repo_name} -s open --json number,title,url --label {label_query}"
+    command = f"gh pr list -R {repo_name} -s open --json number,title,url {label_args}"
     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
     prs_json = result.stdout
 
     prs = json.loads(prs_json)
     return prs
+
 
 if __name__ == "__main__":
     prs = fetch_prs()
