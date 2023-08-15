e922fc475ca9906beaf89088582fa5514e9d75e4
BastianZim <10774221+BastianZim@users.noreply.github.com>
2023-08-15 14:36:11 -0400

    Only return the oldest 20

--- github_handler.py
+++ github_handler.py
@@ -6,12 +6,18 @@ def fetch_prs():
     labels = ["python", "review-requested"]
     label_args = " ".join([f"--label {label}" for label in labels])
 
-    command = f"gh pr list -R {repo_name} -s open --json number,title,url {label_args}"
+    # Include the created field to sort by creation date
+    command = f"gh pr list -R {repo_name} -s open --json number,title,url,created {label_args}"
     result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
     prs_json = result.stdout
 
     prs = json.loads(prs_json)
-    return prs
+
+    # Sort by creation date and take the first 20
+    sorted_prs = sorted(prs, key=lambda x: x['created'])[:20]
+    
+    return sorted_prs
+
 
 
 if __name__ == "__main__":
