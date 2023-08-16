20b76f7d24bb4e2784a3ca8d321b7fe76398bb66
BastianZim <10774221+BastianZim@users.noreply.github.com>
2023-08-16 17:03:18 -0400

    Download names and add token

--- github_handler.py
+++ github_handler.py
@@ -2,7 +2,7 @@
 import subprocess
 import json
 import yaml
-import jinja2
+from jinja2 import Template, Undefined
 import os
 import requests
 
@@ -10,8 +10,9 @@ import requests
 def fetch_prs():
     """Fetch PRs with package names from the specified repository."""
     config = _read_config()
+    token = os.environ.get("CF_REVIEWER")
     sorted_prs = _get_sorted_prs(config)
-    extended_prs = _extract_package_names(sorted_prs, config['repository_name'])
+    extended_prs = _extract_package_names(sorted_prs, config['repository_name'], token)
     return extended_prs
 
 
@@ -43,14 +44,14 @@ def _get_sorted_prs(config):
     return sorted_prs
 
 
-def _extract_package_names(sorted_prs, repo_name):
+def _extract_package_names(sorted_prs, repo_name, token):
     """Extract package names from the sorted PRs."""
     extended_prs = []
     for pr in sorted_prs:
         pr_number = pr['number']
         meta_yaml_files = _get_meta_yaml_files(pr_number, repo_name)
         for meta_yaml_file in meta_yaml_files:
-            meta_yaml_content = _download_meta_yaml_content(pr_number, repo_name, meta_yaml_file)
+            meta_yaml_content = _download_meta_yaml_content(pr_number, repo_name, meta_yaml_file, token)
             package_name = _extract_package_name(meta_yaml_content)
             pr_with_package = pr.copy()
             pr_with_package['package_name'] = package_name
@@ -61,26 +62,50 @@ def _extract_package_names(sorted_prs, repo_name):
 def _get_meta_yaml_files(pr_number, repo_name):
     """Get meta.yaml files from the specified PR number and repository name."""
     command = f"gh pr diff {pr_number} -R {repo_name} --name-only"
-    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True)
+    result = subprocess.run(command, stdout=subprocess.PIPE, shell=True, text=True, check=True)
     added_files = result.stdout.splitlines()
     meta_yaml_files = [f for f in added_files if f.startswith('recipes/') and f.endswith('/meta.yaml')]
     return meta_yaml_files
 
 
-def _download_meta_yaml_content(pr_number, repo_name, meta_yaml_file):
+def _download_meta_yaml_content(pr_number, repo_name, meta_yaml_file, token):
     """Download the content of the specified meta.yaml file."""
-    download_url = f"https://raw.githubusercontent.com/{repo_name}/pull/{pr_number}/head/{meta_yaml_file}"
+    print("Trying to download:", pr_number)
+    # Get the PR details using the GitHub API
+    pr_url = f"https://api.github.com/repos/{repo_name}/pulls/{pr_number}"
+    headers = {'Authorization': f'token {token}'}
+    pr_response = requests.get(pr_url, headers=headers)
+    pr_data = pr_response.json()
+
+    # Get the ref (SHA) of the specific commit in the pull request
+    ref = pr_data['head']['sha']
+
+    # Construct the URL to download the raw content of the meta.yaml file
+    download_url = f"https://raw.githubusercontent.com/{repo_name}/{ref}/{meta_yaml_file}"
     response = requests.get(download_url)
     return response.text
 
 
+class SilentUndefined(Undefined):
+    """Ignore undefined variables."""
+
+    def _fail_with_undefined_error(self, *args, **kwargs):
+        return ''
+
+    __str__ = __call__ = __getattr__ = _fail_with_undefined_error
+
+
+def _render_jinja_template(template_content):
+    """Render the provided Jinja template, ignoring undefined variables."""
+    template = Template(template_content, undefined=SilentUndefined)
+    return template.render()
+
+
 def _extract_package_name(meta_yaml_content):
-    """Extract the package name from the provided meta.yaml content."""
-    template = jinja2.Template(meta_yaml_content)
-    rendered_yaml = template.render()
-    package_info = yaml.safe_load(rendered_yaml)
-    package_name = package_info['package']['name']
-    return package_name
+    """Extract the package name from a meta.yaml file."""
+    rendered_content = _render_jinja_template(meta_yaml_content)
+    yaml_content = yaml.safe_load(rendered_content)
+    return yaml_content['package']['name']
 
 
 # if __name__ == "__main__":
